4bf71db333e20911f092fec39729c06fefd44e22
Leonardo-jfk <melehAmiti@proton.me>
2026-03-12 17:27:35 -0300

    wft

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="transView"
         options={{
           title: "Transactions",
           // title: "Goals",
