36b6c447f59003a63b59a9da2789f1cb044cce29
Leonardo-jfk <melehAmiti@proton.me>
2026-04-26 11:49:43 -0300

    [bug] added supabase keys

--- src/library/supabase.ts
+++ src/library/supabase.ts
@@ -2,8 +2,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://VOTRE_PROJET.supabase.co'; // Remplacez
-const SUPABASE_ANON_KEY = 'VOTRE_CLE_ANON'; // Remplacez
+const SUPABASE_URL = 'https://dkwnjorztpimitpmqspp.supabase.co'; // Remplacez
+const SUPABASE_ANON_KEY = 'sb_publishable_5ZUwfoxfTFwcGnsrBuvbsA_HKagegA9N'; // Remplacez
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
